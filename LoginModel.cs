2c5c0dd0568c346d66f37943088049f25caa0481
TrungPham97 <37767899+TrungPham97@users.noreply.github.com>
2018-04-22 23:14:24 +0700

    User & Login Controller/Model

---
+++ LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace QLKS.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Mời nhập user name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Mời nhập password")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
